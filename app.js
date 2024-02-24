e07197e143e66ad38ff3bdc1a1da342ef9e942d8
Mr-Hasan-Raza <Mr.HasanRaza@otlook.com>
2024-02-24 18:42:13 +0500

    File-Completed

---
+++ app.js
@@ -0,0 +1,176 @@
+// CHAPTER 01
+
+// QUESTION 01
+var alert01 = ("Welcome! to my JavaScript Assignment 01");
+alert(alert01);
+
+// QUESTION 02
+var alert02 = ("Error! Please enter a valid Password.");
+alert(alert02);
+
+// QUESTION 03
+var alert03 = ("Welcome to JS Land...  Happy Coding!");
+alert(alert03);
+
+// QUESTION 04
+var alert04 = ("Welcome to JS Land...");
+var alert04_1 = ("Happy Coding!");
+alert(alert04);
+alert(alert04_1);
+
+// QUETSION 05
+var console01 = ("Hello... I can run JS through my web browser's console");
+console.log(console01);
+
+// QUESTION 06
+var alert05 = ("Done!");
+alert(alert05);
+
+// QUESTION 07
+var alert06 = ("I will do it!");
+alert(alert06);
+
+// CHAPTER 02
+
+// QUESTION 01
+var username = "hasandev";
+
+// QUESTION 02
+var myName = "Muhammad Hasan Raza";
+
+// QUESTION 03
+var message = "Hello World";
+alert(message);
+
+// QUESTION 04
+var studentbio01 = prompt("Whats your name?");
+alert(studentbio01);
+var studentbio02 = prompt("whats your age?");
+alert(studentbio02);
+var studentbio03 = prompt("In which course you enrolled in?");
+alert(studentbio03);
+
+// QUESTION 05
+var eat = ("Pizza");
+alert(eat);
+alert(eat-1);
+alert(eat-2);
+alert(eat-3);
+alert(eat-4);
+
+// QUESTION 06
+var email = ("Whats your email address?");
+alert("My email address is " + email + "@gmail.com");
+
+// QUESTION 07
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// QUESTION 08
+var html = "Yes i can write HTML content through JavaScript";
+document.write(book);
+
+// QUESTION 09
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+
+// CHAPTER 03
+
+// QUESTION 01
+var age = 21;
+alert("I am " + age + "years old.");
+
+// QUESTION 02 
+// NOT LEARN TILL YET
+
+// QUESTION 03
+var birthYear = 2002;
+document.write("My birth year is " + birthYear);
+
+// QUESTION 04
+var customerName = "JOHN DOE";
+var cuustomerProduct = "T-SHIRT";
+var customerQuantity = 5;
+document.write(customerName + " is ordered 5 " + customerQuantity + " " +  cuustomerProduct + " at 1500/= Rs each");
+
+// CHAPTER 04
+
+// QUESTION 01
+var name01 = "Muhammad Hasan Raza" , university = "Ecpertizo University" , course = "Web & Mobile app development";
+document.write("My name is "+ name01 + " student of " + university + " currently enrolled in " + course);
+
+// QUESTION 02
+// NOT UNDERSTOOD
+
+// QUESTION 03
+var rule01 = "Variable names can only contain number , $ , _ and letters . For example $my_1stVariable";
+var rule02 = "Variable must begins with a letters , $ and _ . For example $name , _name or name";
+var rule03 = "Variable names are case sensitive";
+var rule04 = "Varibale names should not be JS Keywords";
+document.write(rule01);
+document.write(rule02);
+document.write(rule03);
+document.write(rule04);
+
+// CHAPTER 05
+
+// QUESTION 01
+var sum01 = +prompt("Give a first number for addition");
+var sum02 = +prompt("Give a second number for addition");
+var sumof02Variables = (sum01 + sum02);
+document.write = ("Sum of " + sum01 + " and "+ sum02 + " is " + sumof02Variables);
+
+// QUESTION 02
+var subtract01 = +prompt("Give a first number for subtraction");
+var subtract02 = +prompt("Give a second number for subtraction");
+var subtractionof02Variables = (subtract01 - studentbio02);
+document.write = ("Sum of " + subtract01 + " and "+ subtract02 + " is " + subtractionof02Variables);
+
+var multiplication01 = +prompt("Give a first number for multiplication");
+var multiplication02 = +prompt("Give a second number for multiplication");
+var multiplicationof02Variables = (multiplication01 * multiplication02);
+document.write = ("Sum of " + multiplication01 + " and "+ multiplication02 + " is " + multiplicationof02Variables);
+
+var division01 = +prompt("Give a first number for division");
+var division02 = +prompt("Give a second number for division");
+var divisionf02Variables = (division01 / division02);
+document.write = ("Sum of " + division01 + " and "+ division02 + " is " + divisionf02Variables);
+
+var modulous01 = +prompt("Give a first number for modulous");
+var modulous02 = +prompt("Give a second number for modulous");
+var modulousof02Variables = (modulous01 % modulous02);
+document.write = ("Sum of " + modulous01 + " and "+ modulous02 + " is " + modulousof02Variables);
+
+// QUESTION 03
+// ICREAMENT AND DECREAMNT NOT LEARN TILL YET 
+
+// QUESTION 04
+var movieticketprice = "600";
+var movieticketquantity = +prompt("How many tickets do you want?");
+var movieticketamount = (movieticketprice * movieticketquantity);
+document.write("Total cost to buy " + movieticketquantity + " tickets to a movie is " + movieticketamount + " " + "PKR");
+
+// QUESTION 05
+var table = +prompt("Write a number to get a tuble");
+document.write("Table of " + table);
+document.write(table + " x 1 =" + " " *table*1);
+document.write(table + " x 2 =" + " " *table*2);
+document.write(table + " x 3 =" + " " *table*3);
+document.write(table + " x 4 =" + " " *table*4);
+document.write(table + " x 5 =" + " " *table*5);
+document.write(table + " x 6 =" + " " *table*6);
+document.write(table + " x 7 =" + " " *table*7);
+document.write(table + " x 8 =" + " " *table*8);
+document.write(table + " x 9 =" + " " *table*9);
+document.write(table + " x 10 =" + " " *table*10);
+
+// QUESTION 06
+var celsius = +prompt("Write a Celsius of your place.");
+var fahernite = +prompt("Write a Fahernite of your place.");
+var celsiusConversion = ((celsius * 9/5) + 32);
+var faherniteConversion = ((fahernite-32) * 5/9);
+document.write(celsius + "O is " + celsiusConversion + "oFahernite");
+document.write(fahernite + "O is " + faherniteConversion + "oCelsius");
+
+// QUESTION 07
+// Not Understood
